3f139577aca720684c0e0f9f64cf3c8db3432422
Dioteng <jonaemazis@gmail.com>
2024-04-30 09:42:21 +0800

    Login UI 04/29

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,19 @@
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @include('layouts.components.head')
 
-    <body class="auth-body-bg">
+    <body class="" style="background: #f4f2f1">
         
         <div class="account-pages my-5 pt-sm-5">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-8 col-lg-6 col-xl-5">
                         <div class="card overflow-hidden">
-                            <div class="bg-primary-subtle">
+                            <div class="" style="background: #022130">
                                 <div class="row">
                                     <div class="col-7">
-                                        <div class="text-primary p-4">
-                                            <h5 class="text-primary">Welcome Back !</h5>
-                                            <p>Sign in to continue to Skote.</p>
+                                        <div class="p-4" style="color: #f4f2f1">
+                                            <h5>LIGTAS<strong style="color: #ffbc2e">GIS</strong></h5>
+                                            <p>Emergency Reporting App</p>
                                         </div>
                                     </div>
                                     <div class="col-5 align-self-end">
@@ -28,7 +28,7 @@
                                     <a href="#" class="auth-logo-light">
                                         <div class="avatar-md profile-user-wid mb-4">
                                             <span class="avatar-title rounded-circle bg-light">
-                                                <img src="assets/images/logo-light.svg" alt="" class="rounded-circle" height="34">
+                                                <img src="assets/images/ligtas_icon.svg" alt="" class="rounded-circle" height="70">
                                             </span>
                                         </div>
                                     </a>
@@ -36,7 +36,7 @@
                                     <a href="#" class="auth-logo-dark">
                                         <div class="avatar-md profile-user-wid mb-4">
                                             <span class="avatar-title rounded-circle bg-light">
-                                                <img src="assets/images/logo.svg" alt="" class="rounded-circle" height="34">
+                                                <img src="assets/images/ligtas_icon.svg" alt="" class="rounded-circle" height="70">
                                             </span>
                                         </div>
                                     </a>
@@ -82,14 +82,14 @@
                                         </div>
                                         
                                         <div class="mt-3 d-grid">
-                                            <button class="btn btn-primary waves-effect waves-light" type="submit">Log In</button>
+                                            <button class="btn waves-effect waves-light" type="submit" style="background: #30e483; color: #f4f2f1">Log In</button>
                                         </div>
             
 
                                         <div class="mt-4 text-center">
                                             <!-- <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a> -->
                                             @if (Route::has('password.request'))
-                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                <a style="color: #ffbc2e" class="btn btn-link" href="{{ route('password.request') }}">
                                                     {{ __('Forgot Your Password?') }}
                                                 </a>
                                             @endif
@@ -100,10 +100,8 @@
                             </div>
                         </div>
                         <div class="mt-5 text-center">
-                            
                             <div>
-                                <p>Don't have an account ? <a href="/register" class="fw-medium text-primary"> Signup now </a> </p>
-                                <p>© <script>document.write(new Date().getFullYear())</script> Skote. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                                <p>Don't have an account ? <a href="/register" class="fw-medium" style="color: #ffbc2e"> Signup now </a> </p>
                             </div>
                         </div>
 
